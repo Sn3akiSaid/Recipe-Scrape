e4ea40e8d22ce1ddbff548875fc032cfb8b7ec62
Sn3akiWSL <taragoshevaleksandar@gmail.com>
2026-04-01 00:59:18 +0100

    Added cleanup methods using re that get rid of special characters. Tidied code and added a helper function instead of repeating same code.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 import re
 
 try:
-	# url_input = input("Input URLS:")
-	url_input = "https://mykoreankitchen.com/korean-beef-bone-broth/"
+	### TEST URLS ###
+	# url_input = "https://mykoreankitchen.com/korean-beef-bone-broth/"
+	url_input = "https://mykoreankitchen.com/korean-fried-chicken/"
 	print(url_input)
 except:
 	print('Invalid!')
@@ -17,32 +18,33 @@ def url_get(url):
 	soup = BeautifulSoup(webpage, 'lxml')
 	return soup
 
-def parse(soup):
+def helpah(data, title_element, list):
+	data[title_element] = ()
+	for ingredient in list:
+		label = soup.find('label',class_='wprm-checkbox-label').text
+		data[title_element] = data[title_element] + (re.sub(label, '',ingredient.text),)
+	return data
 
+def parse(soup):
 	ingredients_lists = soup.find_all('div', class_='wprm-recipe-ingredient-group')
-	data1 = {}
-	data2 = []
-	for titles in ingredients_lists:
+	full_list = {}
+
+	for recipes in ingredients_lists:
+		ingredients = recipes.find(
+			'ul', class_='wprm-recipe-ingredients'
+		)
+		
 		try:
-			title_element = titles.find(
+			sub_recipe = recipes.find(
 				'h4', class_='wprm-recipe-group-name'
 			).text
-			ingredients = titles.find(
-				'ul', class_='wprm-recipe-ingredients'
-			)
 
-			data1[title_element] = ()
-			for ingredient in ingredients:
-				data1[title_element] = data1[title_element] + (ingredient.text,)
-			return data1
+			helpah(full_list, sub_recipe, ingredients)
 	
 		except AttributeError:
-			ingredients = titles.find(
-				'ul', class_='wprm-recipe-ingredients'
-			)
-			for ingredient in ingredients:
-				data2.append(ingredient.text)
-			return data2
+			helpah(full_list, 'Ingredients', ingredients)
+				
+	return full_list
 
 soup = url_get(url_input)
 text = parse(soup)
